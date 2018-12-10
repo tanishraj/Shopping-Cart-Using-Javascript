d33ede88c40e4cdb3f9646c88ea37c178a8f2de6
tanishraj <tanishraj.quitegrowth@gmail.com>
2018-12-10 21:20:36 +0530

    Shopping Cart Completed with delete options

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ cartBody.addEventListener('click', function(evt){
 	var element = evt.target.classList[0];
 	var productQuantity = event.target.parentNode.parentNode.previousElementSibling;
 
+	var totalPrice  = $('.cart-footer .total-amount').text();
+	var currentItemPrice = $(event.target).closest('.product-items').find('.product-amount').text();
+
+
+	console.log("Total price is  = ", currentItemPrice);
+
 	if(element == "increment"){
 		var incrementInput = event.target.previousElementSibling;
 		currentValue = ++incrementInput.value;
+		totalPrice = parseInt(totalPrice) + parseInt(currentItemPrice);
 
 		productQuantity.querySelector('.product-quantity').textContent = currentValue;
 		
@@ -15,5 +22,13 @@ cartBody.addEventListener('click', function(evt){
 		currentValue = --decrementInput.value;
 		
 		productQuantity.querySelector('.product-quantity').textContent = currentValue;
+
+		totalPrice = parseInt(totalPrice) - parseInt(currentItemPrice);
 	}
+
+	$('.cart-footer .total-amount').text(totalPrice);
+})
+
+$('.remove').click(function(){
+	$(this).closest('.product-items').remove();
 })
